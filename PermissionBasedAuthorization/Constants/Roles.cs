793fb89482006c9fc6e12eeea7ec4769fca34aae
Mohammad Habibi Rad <habibiradmohammad@gmail.com>
2022-12-25 16:47:57 +0330

    permission bases authorization completed

---
+++ PermissionBasedAuthorization/Constants/Roles.cs
@@ -0,0 +1,12 @@
+﻿namespace PermissionBasedAuthorization.Constants
+{
+    /// <summary>
+    /// These are the default roles that we would try to seed to the database as soon as the application fires up.
+    /// </summary>
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Basic
+    }
+}
